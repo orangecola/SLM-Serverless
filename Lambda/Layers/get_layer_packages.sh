02f911b3721fcfa6fd9cbe753b218987aeef7078
Joel <orangecola3@gmail.com>
2019-01-08 02:35:29 +0000

    Moved to lambda layers

---
+++ Lambda/Layers/get_layer_packages.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export PKG_DIR="SLS-Serverless-layer/python"
+
+rm -rf ${PKG_DIR} && mkdir -p ${PKG_DIR}
+
+docker run --rm -v $(pwd):/foo -w /foo lambci/lambda:build-python3.6 \
+    pip install -r requirements.txt --no-deps -t ${PKG_DIR}
+
+cp common.py ${PKG_DIR}
